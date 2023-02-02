7efb4ee7f277a913cad5fa40c21c7cfd0c401072
bjh423 <55869855+bjh423@users.noreply.github.com>
2023-02-02 17:34:03 +0900

    Add files via upload

---
+++ day04/out.js
@@ -0,0 +1,23 @@
+function 빨래하다() {
+    console.log("1. 먼지 털기")
+    console.log("2. 입수")
+    console.log("3. 비누칠")
+    console.log("4. 비빈다")
+    console.log("5. 반복 행굼")
+    console.log("6. 탈수")
+    console.log("7. 넌다")
+}
+function 청소하다() {
+    console.log("1. 창문열기")
+    console.log("2. 청소기")
+    console.log("3. 물걸레질")
+}
+
+function 여행하다() {
+    console.log("1. 계획짜기")
+    console.log("2. 실행")
+}
+
+function 쉬다() {
+    console.log("1. 잔다")
+}
